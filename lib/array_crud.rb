f10731a4377efc53992f23e556ba56d771d58c35
Jamie Lawrence-Hurt <jpham1109@gmail.com>
2020-12-27 01:36:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,33 +7,33 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  sake_types << "nama"
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
-  sake_types.unshift("koshu")
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  sake_types.pop()
+  array.pop()
 end
 
 def remove_element_from_start_of_array(array)
-  sake_types.shift()
+  array.shift()
 end
 
 def retrieve_element_from_index(array, index_number)
-  sake_types[index_number]
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  sake_types[0]
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  sake_types[-1]
+  array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-  sake_types[index_number] = element
+  array[index_number] = element
 end
