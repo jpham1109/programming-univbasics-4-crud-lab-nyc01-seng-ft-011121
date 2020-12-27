3a1cbd762618341ba91af82e48b8e4e315fac4e6
Jamie Lawrence-Hurt <jpham1109@gmail.com>
2020-12-27 01:29:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  sake_types = ["honjozo", "junmai", "junmai ginjo", "junmai daiginjo"]
+  sake_types = ["honjozo", "junmai", "ginjo", "daiginjo"]
 end
 
 def add_element_to_end_of_array(array, element)
